fe60c965e126d47665d5ee9e2dff12db45202fe2
Youssef Abbas <youssefabbas.2001.1911@gmail.com>
2023-04-22 17:13:43 +0200

    Add Gaussian Operation

--- Robotics/Control/Fuzzy_Logic/fuzzy_logic_controller.py
+++ Robotics/Control/Fuzzy_Logic/fuzzy_logic_controller.py
@@ -2,6 +2,7 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import numpy as np
 from matplotlib import pyplot as plt
+plt.style.use('fivethirtyeight')
 
 class pidcont():
     def __init__(self,P,I,D,pmax,pmin):
@@ -49,36 +50,40 @@ fig=plt.figure()
 
 ferr = ctrl.Antecedent(np.arange(-150, 150, 1), 'ferr')
 fder = ctrl.Antecedent(np.arange(-150, 150, 1), 'fder')
-fout = ctrl.Consequent(np.arange(-1, 1, 0.01), 'fout')
+fout = ctrl.Consequent(np.arange(-1, 1, 0.01) , 'fout')
 
 ferr.automf(5)
 fder.automf(5)
 fout.automf(5)
-fout['poor'] = fuzz.trimf(fout.universe, [-1, -1, -0.5])
+fout['poor']     = fuzz.trimf(fout.universe, [-1, -1, -0.5])
 fout['mediocre'] = fuzz.trimf(fout.universe, [-1, -0.5, 0])
-fout['average'] = fuzz.trimf(fout.universe, [-0.1, 0, 0.1])
-fout['decent'] = fuzz.trimf(fout.universe, [0, 0.5, 2])
-fout['good'] = fuzz.trimf(fout.universe, [0.5, 1, 1])
-fout.view()
-ferr.view()
-fder.view()
-plt.show()
-plt.pause(0.0001)
+fout['average']  = fuzz.trimf(fout.universe, [-0.1, 0, 0.1])
+fout['decent']   = fuzz.trimf(fout.universe, [0, 0.5, 2])
+fout['good']     = fuzz.trimf(fout.universe, [0.5, 1, 1])
+# fout.view()
+# ferr.view()
+# fder.view()
+# plt.show()
+# plt.pause(0.0001)
 
 #'poor'; 'mediocre'; 'average'; 'decent', or 'good'
 rules=[]
-rules.append(ctrl.Rule(ferr['average'] | fder['average'] , fout['average']))
-rules.append(ctrl.Rule(ferr['decent'] | fder['decent'] , fout['decent']))
-rules.append(ctrl.Rule(ferr['good'] | fder['good'] , fout['good']))
+rules.append(ctrl.Rule(ferr['average']  | fder['average']  , fout['average']))
+rules.append(ctrl.Rule(ferr['decent']   | fder['decent']   , fout['decent']))
+rules.append(ctrl.Rule(ferr['good']     | fder['good']     , fout['good']))
 rules.append(ctrl.Rule(ferr['mediocre'] | fder['mediocre'] , fout['mediocre']))
-rules.append(ctrl.Rule(ferr['poor'] | fder['poor'] , fout['poor']))
+rules.append(ctrl.Rule(ferr['poor']     | fder['poor']     , fout['poor']))
+
 
 fctrl = ctrl.ControlSystem(rules)
 fpid = ctrl.ControlSystemSimulation(fctrl)
 
-pid= pidcont(1.2,0.02,0.01,5,-5)
+# pid= pidcont(1.2,0.02,0.01,5,-5)
+# pid2= pidcont(1.2,0.02,0.01,5,-5)
 
-pid2= pidcont(1.2,0.02,0.01,5,-5)
+pid= pidcont(.12,0.02,0.01,5,-5)
+pid2= pidcont(.12,0.02,0.01,5,-5)
+
 
 d=np.zeros(10)
 for i in range(10):
@@ -97,14 +102,18 @@ de2=[]
 kp=pid.kp
 kd=pid.kd
 ki=pid.ki
+
 for i in range(len(d)):
     pid.setDesired(d[i])
+
+    print("\n\n")
     print("e:",pid.error ,"\t de:", pid.eder)
     fpid.input['ferr'] = pid.error
     fpid.input['fder'] = pid.eder
     fpid.compute()
-    newpid=np.abs(fpid.output['fout'])
-    print("PID:", newpid*pid.kp,"\t",newpid*pid.ki,"\t",newpid*pid.kd)
+
+    newpid = np.abs(fpid.output['fout'])
+    print(f"PID: KP: {newpid*pid.kp},  KI: {newpid*pid.ki},  KD: {newpid*pid.kd}")
     pid.setGains(newpid*kp,newpid*ki,newpid*kd)
     newm=pid.update(m[-1])
     newm=m[-1]+newm
@@ -124,17 +133,19 @@ for i in range(len(d)):
     ax1.set_xlim([0, len(d)])
     ax1.set_ylim([-200, 200])
     plt.grid()
-    plt.plot(range(len(m)),m,linewidth=5.0)
-    plt.plot(range(len(m2)),m2,linewidth=2.0)
-    plt.plot(range(len(d)),d,'g--')
-
+    plt.plot(range(len(m)) ,m , 'r'  ,linewidth=1.5 , label='fuzzyPID')
+    plt.plot(range(len(m2)),m2, 'b'  ,linewidth=1.5 , label='classicPID')
+    plt.plot(range(len(d)) ,d , 'g--',linewidth=1, label='setpoint')
+    # plt.legend()
     plt.title('Status')
+
     ax2=plt.subplot(2,1,2)
     ax2.set_xlim([0, 50])
     ax2.set_ylim([-100, 100])
     plt.plot(range(len(e)),e,'r-',range(len(de)),de,'g-')
     plt.grid()
     plt.title('e and ed')
+
     #plt.draw()
     plt.show()
-    plt.pause(0.0001)
+    plt.pause(0.1)
