ffde2962e5a86dd8a09002ab884d9792a34514f4
Youssef Abbas <youssefabbas.2001.1911@gmail.com>
2023-06-07 14:19:17 +0300

    Add GA tutorial

--- Operation Research/optimization/genetic_algorithm/examples/maxone.py
+++ Operation Research/optimization/genetic_algorithm/examples/maxone.py
@@ -10,10 +10,12 @@ def onemax(x):
 def selection(pop, scores, k=3):
 	# first random selection
 	selection_ix = randint(len(pop))
+	
 	for ix in randint(0, len(pop), k-1):
 		# check if better (e.g. perform a tournament)
 		if scores[ix] < scores[selection_ix]:
 			selection_ix = ix
+			
 	return pop[selection_ix]
 
 # crossover two parents to create two children
@@ -41,19 +43,24 @@ def mutation(bitstring, r_mut):
 def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut):
 	# initial population of random bitstring
 	pop = [randint(0, 2, n_bits).tolist() for _ in range(n_pop)]
+	
 	# keep track of best solution
 	best, best_eval = 0, objective(pop[0])
+	
 	# enumerate generations
 	for gen in range(n_iter):
 		# evaluate all candidates in the population
 		scores = [objective(c) for c in pop]
+		
 		# check for new best solution
 		for i in range(n_pop):
 			if scores[i] < best_eval:
-				best, best_eval = pop[i], scores[i]
-				print(">%d, new best f(%s) = %.3f" % (gen,  pop[i], scores[i]))
+				gen, best, best_eval = gen, pop[i], scores[i]
+				print("gen:%d  best  -> f(%s) = %.3f" % (gen,  pop[i], scores[i]))
+				
 		# select parents
 		selected = [selection(pop, scores) for _ in range(n_pop)]
+		
 		# create the next generation
 		children = list()
 		for i in range(0, n_pop, 2):
@@ -65,21 +72,29 @@ def genetic_algorithm(objective, n_bits, n_iter, n_pop, r_cross, r_mut):
 				mutation(c, r_mut)
 				# store for next generation
 				children.append(c)
+				
 		# replace population
 		pop = children
-	return [best, best_eval]
+		
+	return [gen, best, best_eval]
 
 # define the total iterations
 n_iter = 100
+
 # bits
-n_bits = 20
+n_bits = 10
+
 # define the population size
-n_pop = 100
+n_pop = 6
+
 # crossover rate
-r_cross = 0.9
+r_cross = 0.6
+
 # mutation rate
 r_mut = 1.0 / float(n_bits)
+
 # perform the genetic algorithm search
-best, score = genetic_algorithm(onemax, n_bits, n_iter, n_pop, r_cross, r_mut)
-print('Done!')
-print('f(%s) = %f' % (best, score))
+gen, best, score = genetic_algorithm(onemax, n_bits, n_iter, n_pop, r_cross, r_mut)
+print('Done!\n')
+print('Final Result:')
+print(f'(gen:{gen}) f({best}) = {score}' )
